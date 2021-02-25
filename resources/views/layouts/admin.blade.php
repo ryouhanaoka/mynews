f35c0f2c2e2e1d9e365ceca551c84735cf4cb484
ryouhanaoka <i.ain't.alive.without.bass@gmail.com>
2021-02-25 14:22:32 +0000

    situmon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -49,49 +49,33 @@ expanded="false" aria-label="Toggle navigation">
 icon"></span>
                     </botton>
                     
-                    <div class= "collapse navbar-
-collapse" id="navbarSupportedContent">
+                    <div class= "collapse navbar-collapse" id="navbarSupportedContent">
                         <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-
-auto">
+                        <ul class="navbar-nav mr-auto">
                         </ul>
                         
                         <!--Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-
-auto">
+                        <ul class="navbar-nav ml-auto">
                         @guest
-                            <li><a class="nav-link"
-href="{{ route('login') }}">{{ __('Login') }}</a></li>
-
+                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                        
                         @else
-                            <li class="nav-item
-dropdown">
-                                <a id="navbarDropdown"
-class="nav-link dropdown-toggle" href="#"
-role="button" data-toggle="dropdown" aria-
-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-<span class="caret"></span>                    
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-                                
-                                <div class="dropdown-
-menu" aria-labelledby="navbarDropdown">
-                                    <a
-class="dropdown-item" href="{{ route('logout') }}" 
 
-onclick="event.preventDefault();
-
-document.getElementById('logout-form').submit();">
-                                        {{
-__('Logout') }}
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
                                     </a>
-                                    
-                                    <form id="logout-
-form" action="{{ route('logout') }}" method="POST"
-style="display: none;">
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
-                                </div> 
+                                </div>
                             </li>
                             @endguest
                         </ul>
