f3f76491ab3accd62913817c053d100f1e1ae504
ryouhanaoka <i.ain't.alive.without.bass@gmail.com>
2021-02-25 04:58:27 +0000

    kadai11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible"
-content="IE=edge">
-        <meta name="viewport" content="width=device-
-width, initial-scale=1">
-
-        <title>profile</title>
-    </head>
-    <body>
-
-        
     @extends('layouts.profile')
         @section('title','My プロフィール')
         
         @section('content')
-        <div class="contener">
+        <div class="container">
             <div class="row">
                 <div>
                     <h2>プロフィール</h2>
@@ -24,5 +10,4 @@ width, initial-scale=1">
             </div>
         </div>
     @endsection
-    </body>
-</html>
+   
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 Route::group(['prefix' => 'admin'] , function() {
     Route::get('news/create', 
-'Admin\NewsController@add');
+'Admin\NewsController@add')->middleware('auth');
 });
 
 //課題3//
@@ -28,3 +28,6 @@ Route::get('admin/profile/create',
 
 Route::get('admin/profile/edit',
 'Admin\ProfileController@edit');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
