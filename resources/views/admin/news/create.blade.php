8ce1f3075b25c8bef18db380b7d45d6dd4333e84
ryouhanaoka <i.ain't.alive.without.bass@gmail.com>
2021-02-25 09:35:28 +0000

    situmonn

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,22 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible"
-content="IE=edge">
-        <meta name="viewport" content="width=device-
-width,　initial-scale=1">
 
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>Myニュース制作画面</h1>
         
     @extends('layouts.admin')
         @section('title', 'ニュースの新規作成')
         
         @section('content')
-        <div class="conteiner">
+        <div class="container">
             <div class="row">
                 <div class="col-md-8 mx-auto">
                     <h2>ニュースの新規作成</h2>
