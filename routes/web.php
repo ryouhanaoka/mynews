f7764e230c1588adc7c7bc08c36d175b566bb383
ryouhanaoka <i.ain't.alive.without.bass@gmail.com>
2021-03-10 07:34:49 +0000

    situmon17

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news', 'Admin\NewsController@index');
     Route::get('news/edit', 'Admin\NewsController@edit');
     Route::post('news/edit', 'Admin\NewsController@update');
+    Route::get('news/delete', 'Admin\NewsController@delete');
 });
 
 //練習//
@@ -37,8 +38,10 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth' ], function() {
 Route::group(['prefix' => 'admin', 'middleware' => 'auth' ], function() {
 Route::get('profile/edit', 'Admin\ProfileController@edit');
 Route::post('profile/edit', 'Admin\ProfileController@update');
+    
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
